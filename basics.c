60460ac9151c6f1096edc11d1c614e959a3deef9
oveey <ovieigere@gmail.com>
2023-03-23 13:28:55 +0100

    feat:made a new C file

---
+++ basics.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello world\n");
+    printf("I am learning C.\t");
+    printf("\ni am a \" boy");
+    return 0;
+     //Single Line Comment
+    //This is the beginner C code 
+    //Multi Line Comment 
+    /*
+    this is a multi line comment
+    this is a multi line comment 
+    this is a mullt line comment 
+    */
+}
